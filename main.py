a4e67322a8e02ebccb000394238cd83ef9b913df
‪Omar_Bahous‬‏ <omarbahous88@gmail.com>
2026-03-21 18:47:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class caracter(BaseModel):
     attack : int 
     defence : int 
     image_url: str
-caracters=[caracter(HP=3000,name="Gojo",type="sorcer",attack=5000,defence=4000,image_url="jujutsu_kaisen_gojo.png")]
+caracters=[caracter(HP=3000,name="Gojo",type="sorcer",attack=5000,defence=4000,image_url="https://pinkcrow.net/wp-content/uploads/2025/09/Satoru_Gojo_arrives_on_the_battlefield_29-1.jpg")]
 @app.get("/caracters/")
 def readcaracters():
     return caracters
